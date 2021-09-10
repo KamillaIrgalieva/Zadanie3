f6a33658492e7329d8b5aa25e6d7e3dea433bf03
IrgalievaKM <kama270290@yandex.ru>
2021-09-10 22:47:11 +0300

    Initial commit

---
+++ src/com/company/StreamMain.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamMain {
+    List<Integer> intList2 = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+}
+
+
+
+
